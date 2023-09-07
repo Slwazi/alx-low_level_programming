c5be0b3588f85924689fac8c8719607b2e4a8fe4
Lwazi <slwazit@gmail.com>
2023-09-07 16:10:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * thsi script will print
+ * with prpoper
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
