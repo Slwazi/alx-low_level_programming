24d071a40182f924800fbe2612bade5010be6599
Lwazi <slwazit@gmail.com>
2023-09-07 16:05:11 -0700

    i love this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that
+ * prints a message and returns 0.
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-	/* AM happy today */
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
