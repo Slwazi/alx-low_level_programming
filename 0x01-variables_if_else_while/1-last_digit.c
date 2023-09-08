a73f81ab095d0843fee40ca175fb98b4c4c2070d
Lwazi <slwazit@gmail.com>
2023-09-08 14:02:00 -0700

    WOWO

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,35 +6,28 @@
  * main - Entry point of the program
  *
  * Description: This function initializes a random number and prints whether
- *						it's greater than 5, and is 0, or less than 6 and not 0.
+ * it's greater than 5, and is 0, or less than 6 and not 0.
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
+
 	int n;
 
 	srand(time(0));
-	n = rand();
-
-	int lastDigit = n % 10;
-
-	printf("Last digit of %d is %d", n, lastDigit);
-
-	if (lastDigit > 5)
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is ", n);
+	if (n > 5)
 	{
-		printf(" and is greater than 5\n");
-	}
-	else if (lastDigit == 0)
+		printf("and is greater than 5\n");
+	} else if (n == 0)
 	{
-		printf(" and is 0\n");
-	}
-	else
+		printf("and is 0\n");
+	} else if (n < 6 && n != 0)
 	{
-		printf(" and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 	}
-
 	return (0);
 }
-
