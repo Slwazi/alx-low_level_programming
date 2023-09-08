5767e6e92a6a79858a5a766c84272dfb3a29c9c2
Lwazi <slwazit@gmail.com>
2023-09-08 16:14:53 -0700

    kk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,20 +12,20 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar(i + '0');
+		if(i < 0)
 		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-			putchar(j + '0');
-			putchar(' ');
+			putchar('.');
+			putchar('.');
 		}
 	}
+
 	putchar('\n');
+	
 	return (0);
 }
 
