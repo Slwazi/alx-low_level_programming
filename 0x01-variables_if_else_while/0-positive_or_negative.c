4e067025f1b10d4c614c729dc4383c80b6df7220
Lwazi <slwazit@gmail.com>
2023-09-08 12:30:04 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point of the program
- *
- * Description: This program prints the sizes of various data types.
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	printf("The number %d is ", n);
-	
-	if (n > 0) {
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    printf("The number %d is ", n);
+
+    if (n > 0) {
         printf("positive\n");
-   
-	} else if (n == 0) {
-
+    } else if (n == 0) {
         printf("zero\n");
-    
-	} else {
+    } else {
         printf("negative\n");
-    
-	}
+    }
 
-	return 0;
+    return (0);
 }
+
