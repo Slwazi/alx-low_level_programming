e18dca41cefc613db75ead27d116c5645f06b84e
Lwazi <slwazit@gmail.com>
2023-09-08 15:10:02 -0700

    Forever

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function the alphabet in lowercase, and then in
+ * uppercase, followed by a new line
+ * it's greater than 5, and is 0, or less than 6 and not 0.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
