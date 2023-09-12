d04c8894f14c900f8d99e86e8ef2d0302b8d4b3d
Lwazi <slwazit@gmail.com>
2023-09-12 12:09:47 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - Entry point of the program
  *
  * Description: This function initializes a random number and prints whether
- *                        it's greater than 5, and is 0, or less than 6 and not 0.
+ * it's greater than 5, and is 0, or less than 6 and not 0.
  *
  * Return: Always 0 (success)
  */
