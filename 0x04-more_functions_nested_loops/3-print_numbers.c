5a6face112d8043981b400114528e6d5deabc7ca
Lwazi <slwazit@gmail.com>
2023-09-14 13:07:31 -0700

    Lwazi

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,14 @@
   *
   * Return: The numbers since 0 up to 9
   */
-
 void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (int i = 0; i <= 9; i++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(i);
+		_putchar(c + '0');
 	}
+
 	_putchar('\n');
 }
