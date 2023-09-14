a28a2a97d72c6ef68728c7fc636f4ff31a2e2cd8
Lwazi <slwazit@gmail.com>
2023-09-14 12:51:20 -0700

    Lwazi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - Check if that multiplies two integers
+  * @a: The number of multiply
+  * @b: second number multiply
+  *
+  * Return: Result of the multiplication
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
