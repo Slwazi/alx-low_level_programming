723b81dd50a6b50e15384120aebb3683857aa630
Lwazi <slwazit@gmail.com>
2023-09-14 14:16:24 -0700

    Lwazi

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
+void print_number(int n)
+{
+	unsigned int v = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		v = n;
+		_putchar('-');
+	}
+
+	v /= 10;
+
+	if (v != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+}
