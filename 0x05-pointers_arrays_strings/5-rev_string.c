65ba8a746e7b48373ffbbf62036e982cabe9dfe4
Lwazi <slwazit@gmail.com>
2023-09-18 05:34:27 -0700

    Lwazi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, lenght, lenght1;
+
+	lenght = 0;
+	lenght1 = 0;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	lenght1 = lenght - 1;
+
+	for (i = 0; i < lenght / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[lenght1];
+		s[lenght1--] = tmp;
+	}
+}
