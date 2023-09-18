d5ab44b37628a159acb68ed657d7878710902bf9
Lwazi <slwazit@gmail.com>
2023-09-18 05:10:52 -0700

    String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: - pointer to the variable to update
+ * the value it points to to 98
+ * Return: s
+ */
+
+int _strlen(char *s);
+{
+	int length = 0;
+
+	while (*s != '\0')
+	[
+		length++;
+	}
+
+	return (length);
+}
