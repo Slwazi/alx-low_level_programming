ce2c133c1735778dd96c1791d1bda37a264878f8
Lwazi <slwazit@gmail.com>
2023-09-27 02:53:26 -0700

    Lwazi

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,11 +6,30 @@
  *
  * Return: the square root of n.
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int low, int high)
 {
-	if (n < 0)
-		return (-1);
 	if (n == 0 || n == 1)
-		return (n);
-	return (n, 2);
+	{
+		return n;
+	}
+
+	int mid = low + (high - low) / 2;
+	int square = mid * mid;
+
+	if (square == n)
+	{
+		return mid;
+	} else if (square < n)
+	{
+		if ((mid + 1) * (mid + 1) > n)
+		{
+			return mid;
+		} else
+		{
+			return _sqrt_recursion(n, mid + 1, high);
+		}
+    } else
+    {
+    	return _sqrt_recursion(n, low, mid - 1);
+    }
 }
