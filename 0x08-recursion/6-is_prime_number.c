a159846101117df01dba37c661aa0c7b4d57896f
Lwazi <slwazit@gmail.com>
2023-09-27 03:23:48 -0700

    Lwazi

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,37 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * is_prime_number - Function if the input integer is a prime number.
- * @n: base number.
+ * prime_number - finds out if a number is prime
  *
- * Return: 1 if the input integer is a prime number otherwise return 0.
+ * @xx: number to determine if prime
+ * @yy: number to check if divisor
+ *
+ * Return: 1 if prime, 0 otherwise
  */
-
+int prime_finder(int xx, int yy)
+{
+	if (xx == yy)
+		return (1);
+	if (!(xx % yy))
+		return (0);
+	return (prime_number(xx, yy + 1));
+}
+/**
+ * is_prime_number - returns if a number is prime
+ *
+ * @n: number to determine its prime
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return 0;
-    }
-    if (n <= 3)
-    {
-    	return 1;
-    }
-
-
-    if (n % 2 == 0 || n % 3 == 0)
-    {
-    	return 0;
-    }
-
-
-    for (int i = 5; i * i <= n; i += 6)
-    {
-    	if (n % i == 0 || n % (i + 2) == 0)
-    	{
-    		return 0;
-    	}
-    }
-
-    return 1;
+	if (n < 2)
+		return (0);
+	return (prime_number(n, 2));
 }
