cb397f00167c4c6a85317ff2831a332b763a99ff
Lwazi <slwazit@gmail.com>
2023-09-27 03:17:10 -0700

    Lwazi

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,30 @@
 #include "main.h"
-
 /**
- * _sqrt_recursion - Function calculates the natural square root of n.
+ * get_root - calculates i**i  n.
+ * @n: base number.
+ * @i: iterator number.
+ * Return: i.
+ */
+int get_root(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i <= n)
+		return (get_root(n, i + 1));
+	else
+		return (-1);
+}
+/**
+ * _sqrt_recursion - calculates the natural square root of n.
  * @n: base number.
  *
  * Return: the square root of n.
  */
-int _sqrt_recursion(int n, int low, int high)
+int _sqrt_recursion(int n)
 {
+	if (n < 0)
+		return (-1);
 	if (n == 0 || n == 1)
-	{
-		return n;
-	}
-
-	int mid = low + (high - low) / 2;
-	int square = mid * mid;
-
-	if (square == n)
-	{
-		return mid;
-	} else if (square < n)
-	{
-		if ((mid + 1) * (mid + 1) > n)
-		{
-			return mid;
-		} else
-		{
-			return _sqrt_recursion(n, mid + 1, high);
-		}
-    } else
-    {
-    	return _sqrt_recursion(n, low, mid - 1);
-    }
+		return (n);
+	return (get_root(n, 2));
 }
