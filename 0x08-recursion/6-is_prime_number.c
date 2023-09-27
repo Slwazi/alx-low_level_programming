fcf87ee374bc77975ae5a8172b28ee95d7359484
Lwazi <slwazit@gmail.com>
2023-09-27 03:10:19 -0700

    Lwazi

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_prime_number - Function if the input integer is a prime number.
+ * @n: base number.
+ *
+ * Return: 1 if the input integer is a prime number otherwise return 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return 0;
+    }
+    if (n <= 3)
+    {
+    	return 1;
+    }
+
+
+    if (n % 2 == 0 || n % 3 == 0)
+    {
+    	return 0;
+    }
+
+
+    for (int i = 5; i * i <= n; i += 6)
+    {
+    	if (n % i == 0 || n % (i + 2) == 0)
+    	{
+    		return 0;
+    	}
+    }
+
+    return 1;
+}
