33da92a185e8b59a8007060792524a5e35c43d53
Lwazi <slwazit@gmail.com>
2023-09-29 12:17:48 -0700

    Lwazi

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all arguments it receives
+ *        All arguments should be printed, including the first on
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+
+	for (c = 0; c < argc; c++)
+	{
+		printf("%s\n", argv[c]);
+	}
+	return (0);
+}
